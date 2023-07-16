a42b6b75d090a768b2d5d481960eb5dc5ecdaa3f
Nuku <dms.silver@gmail.com>
2023-07-16 11:08:46 -0300

    addtrack bug fix

--- module/token.js
+++ module/token.js
@@ -10,22 +10,25 @@ function fillOptions(html, options) {
 }
 let menuTab = false;
 function addTab(tokenConfig, html, data) {
-    const musicList = tokenConfig.token.getFlag(SYSTEM_ID, 'musicList') ?? [['', 100]];
-    const resource = tokenConfig.token.getFlag(SYSTEM_ID, 'resource');
+    const token = tokenConfig.preview;
+    const musicList = token.getFlag(SYSTEM_ID, 'musicList') ?? [['', 100]];
+    const resource = token.getFlag(SYSTEM_ID, 'resource');
     data['trackedResource'] = resource
-        ? tokenConfig.token.getBarAttribute?.('tracked-resource', {
+        ? token.getBarAttribute?.('tracked-resource', {
             alternative: resource,
         })
-        : tokenConfig.token.getBarAttribute('bar1');
+        : token.getBarAttribute('bar1');
     data['completeAttributes'] = { 'Attribute Bars': data.barAttributes['Attribute Bars'] };
     data['trackSelection'] = musicList.map((music, index) => ({ threshold: music[1], disabled: index === 0 }));
-    data['musicPriority'] = tokenConfig.token.getFlag(SYSTEM_ID, 'priority') ?? 10;
-    data['turnOnly'] = tokenConfig.token.getFlag(SYSTEM_ID, 'turnOnly') ?? false;
+    data['musicPriority'] = token.getFlag(SYSTEM_ID, 'priority') ?? 10;
+    data['turnOnly'] = token.getFlag(SYSTEM_ID, 'turnOnly') ?? false;
     html[0].querySelector('nav.sheet-tabs.tabs').appendChild($(menu)[0]);
     function selectPlaylist(ev) {
         const playlist = game.playlists.get(ev.target.value);
         const tracks = playlist?.sounds.contents ?? [];
-        this.parentElement.parentElement.querySelector('select[name=track]').innerHTML = [undefined, ...tracks].map((track) => createOption(track)).join('');
+        this.parentElement.parentElement.querySelector('select[name=track]').innerHTML = [undefined, ...tracks]
+            .map((track) => createOption(track))
+            .join('');
     }
     function actionButton(event) {
         event.preventDefault();
@@ -44,7 +47,9 @@ function addTab(tokenConfig, html, data) {
                 tracks.splice(idx, 1);
                 break;
         }
-        tokenConfig._previewChanges({ [`flags.${SYSTEM_ID}`]: { musicList: tracks, priority, resource: resourceEl.value, turnOnly: turnOnlyEl.checked } });
+        tokenConfig._previewChanges({
+            [`flags.${SYSTEM_ID}`]: { musicList: tracks, priority, resource: resourceEl.value, turnOnly: turnOnlyEl.checked },
+        });
         menuTab = true;
         tokenConfig.render();
     }
@@ -84,7 +89,10 @@ function addTab(tokenConfig, html, data) {
         const playlist = 'error' in selected ? undefined : (selected?.parent ?? selected);
         const track = playlist === selected ? undefined : selected;
         const tracks = playlist ? playlist.sounds.contents : [];
-        fillOptions(playlistEl, [undefined, ...combatPlaylists.map((p) => ({ id: p.id, name: p.name, selected: p === playlist }))]);
+        fillOptions(playlistEl, [
+            undefined,
+            ...combatPlaylists.map((p) => ({ id: p.id, name: p.name, selected: p === playlist })),
+        ]);
         fillOptions(trackEl, [undefined, ...tracks.map((p) => ({ id: p.id, name: p.name, selected: p === track }))]);
         playlistEl.addEventListener('change', selectPlaylist);
     }

00b9e4ec366bf1ea5f76562a469542790682f82c
elizeuangelo <elizeu90@gmail.com>
2024-09-12 18:35:58 -0300

    remove js

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,5 @@
 declare global {
-	interface LenientGlobalVariableTypes {
-		canvas: never;
-		game: never;
-		socket: never;
-		ui: never;
-	}
 	interface Window {
-		ForgeVTT: any;
 		CombatMusicMaster: any;
 	}
 }

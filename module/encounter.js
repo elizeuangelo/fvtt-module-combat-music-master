cc22274537314dc6d2cabac0d838d3db3786159d
Nuku <dms.silver@gmail.com>
2024-09-12 18:45:27 -0300

    transformed into js project

--- module/encounter.js
+++ module/encounter.js
@@ -1,8 +1,8 @@
-import { parseMusic, setCombatMusic, stringifyMusic, updateCombatMusic, updateTurnMusic } from './music-manager.js';
+import { parseMusic, stringifyMusic, updateTurnMusic } from './music-manager.js';
 import { SYSTEM_ID } from './settings.js';
 import { createOption } from './token.js';
 
-class CombatTrackerMusicManager extends FormApplication<FormApplicationOptions, any, any> {
+class CombatTrackerMusicManager extends FormApplication {
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
 			id: 'combat-master-tracker',
@@ -13,60 +13,44 @@ class CombatTrackerMusicManager extends FormApplication<FormApplicationOptions, 
 		});
 	}
 
-	/**
-	 * Get all game settings related to the form, to display them
-	 * @param _options
-	 */
-	async getData(_options: any) {
-		const selected = parseMusic(game.combat!.getFlag(SYSTEM_ID, 'overrideMusic') as string);
-		const playlist = 'error' in selected ? undefined : ((selected?.parent ?? selected) as Playlist);
+	async getData(_options) {
+		const selected = parseMusic(game.combat.getFlag(SYSTEM_ID, 'overrideMusic'));
+		const playlist = 'error' in selected ? undefined : selected?.parent ?? selected;
 		const track = playlist === selected ? undefined : selected;
-
 		const tracks = playlist ? playlist.sounds.contents : [];
-
 		return {
-			playlists: game
-				.playlists!.contents.filter((p) => p.getFlag(SYSTEM_ID, 'combat'))
+			playlists: game.playlists.contents
+				.filter((p) => p.getFlag(SYSTEM_ID, 'combat'))
 				.map((p) => ({ id: p.id, name: p.name, selected: p === playlist })),
 			tracks: tracks.map((t) => ({ id: t.id, name: t.name, selected: t === track })),
 		};
 	}
 
-	/**
-	 * Updates the settings to match the forms
-	 * @param _event
-	 * @param formData The form data to be saved
-	 */
-	protected async _updateObject(_event: Event, formData: { playlist: string; track: string }) {
-		const playlist = game.playlists!.get(formData.playlist);
+	async _updateObject(_event, formData) {
+		const playlist = game.playlists.get(formData.playlist);
 		const track = playlist?.sounds.get(formData.track);
 		const sound = stringifyMusic(track ?? playlist);
-
 		game.combat
 			?.update({
 				[`flags.${SYSTEM_ID}.overrideMusic`]: sound,
 			})
 			.then(() => {
-				if (game.combat!.started) updateTurnMusic(game.combat!);
+				if (game.combat.started) updateTurnMusic(game.combat);
 			});
 	}
 
-	protected _activateCoreListeners(html: JQuery<HTMLElement>): void {
+	_activateCoreListeners(html) {
 		super._activateCoreListeners(html);
-
 		function selectPlaylist(ev) {
-			const playlist = game.playlists!.get(ev.target.value);
+			const playlist = game.playlists.get(ev.target.value);
 			const tracks = playlist?.sounds.contents ?? [];
-
-			html[0].querySelector('select[name=track]')!.innerHTML = [undefined, ...tracks]
+			html[0].querySelector('select[name=track]').innerHTML = [undefined, ...tracks]
 				.map((track) => createOption(track))
 				.join('');
 		}
-
-		html[0].querySelector('select[name=playlist]')!.addEventListener('change', selectPlaylist);
+		html[0].querySelector('select[name=playlist]').addEventListener('change', selectPlaylist);
 	}
 }
-
 const button = `
 <a class="combat-button combat-control" data-tooltip="Set Encounter Music" data-control="musicManager">
 <i class="fas fa-music"></i>
@@ -76,12 +60,12 @@ function clickButton() {
 	new CombatTrackerMusicManager().render(true);
 }
 
-function addButton(encounter: CombatTracker, html: JQuery<HTMLElement>, data: CombatTracker.Data) {
+function addButton(_encounter, html) {
 	const title = html[0].querySelector('.encounter-title.noborder');
 	const btn = $(button)[0];
 	if (!game.combat) btn.setAttribute('disabled', '');
 	btn.addEventListener('click', clickButton);
-	title!.insertAdjacentElement('beforebegin', btn);
+	title.insertAdjacentElement('beforebegin', btn);
 }
 
 Hooks.on('renderCombatTracker', addButton);
